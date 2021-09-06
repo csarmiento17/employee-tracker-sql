a19a74abdc292f063417663c36bcad29534c2b9e
jsarmiento17 <jay@stonewellfinancial.com>
2021-09-05 22:56:40 -0400

    Added DB, Schema and Seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,43 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('HR' ),
+  ('Finance'),
+  ('Admin')
+  
+INSERT INTO employee_role
+  (title, salary,department_id)
+VALUES
+  ('Software Engineer',60000,1),
+  ('Sales',40000,2),
+  ('Human Resources',45000,3),
+  ('Accountant',45000,4),
+  ('Admin',30000,5),
+
+INSERT INTO employee
+  (first_name, last_name, role_id,manager_id)
+VALUES
+  ('James', 'Fraser', 'jf@goldenbough.edu',1,1),
+  ('Jack', 'London', 'jlondon@ualaska.edu',1,1),
+  ('Robert', 'Bruce', 'rbruce@scotland.net',1,1),
+  ('Peter', 'Greenaway', 'pgreenaway@postmodern.com',4,2),
+  ('Derek', 'Jarman', 'djarman@prospectcottage.net',4,2),
+  ('Paolo', 'Pasolini', 'ppasolini@salo.com',2,3),
+  ('Heathcote', 'Williams', 'hwilliams@bafta.com',2,3),
+  ('Sandy', 'Powell', 'spowell@oscars.com',2,3),
+  ('Emil', 'Zola', 'ezola@requin.com',3,5),
+  ('Sissy', 'Coalpits', 'scoalpits@greenaway.com',3,5),
+  ('Antoinette', 'Capet', 'acapet@dontloseyourhead.com',3,5),
+  ('Samuel', 'Delany', 'sdelany@dhalgren.com',3,5),
+  ('Tony', 'Duvert', 'tduvert@frenchletters.edu','Admin',5),
+  ('Dennis', 'Cooper', 'dcooper@georgemiles.com','Admin',5),
+  ('Monica', 'Bellucci', 'mbell@irreverisble.net','Admin',5),
+  ('Samuel', 'Johnson', 'sjohnson@boswell.com','Admin',5),
+  ('John', 'Dryden', 'jdryden@restoration.net','Admin',5),),
+  
+
+
+  
+
