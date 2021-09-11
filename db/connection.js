e6b9ba7324591e090e961b7173c385a278fbe3c9
jsarmiento17 <jay@stonewellfinancial.com>
2021-09-11 02:52:19 -0400

    Completed the challenge and some of bonus part

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
-
+const chalk = require("chalk");
 const db = mysql.createConnection({
   host: "localhost",
   // Your MySQL username,
   user: "root",
   // Your MySQL password
-  password: "P@ssw0rd",
+  password: "P@ssw0rd", //process.env.DB_PASSWORD,
   database: "employee_tracker",
 });
 
+console.log(chalk.blue.bold("==Connected to employee_tracker Database=="));
 module.exports = db;
